9953eab0fbfcc2102b1fa67c3559d45b96b3112b
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-10-19 14:37:01 -0400

    nextauth and tailwind working

---
+++ faves/pages/login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+const login = () => {
+    const {data: session} = useSession()
+
+  if (session) {
+    return (
+      <div>
+        <p>welcome, {session.user.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+      
+      )
+  } else {
+    return (
+      <div>
+        <p>You are not signed in.</p>
+        <button onClick={() => signIn()}>Sign in</button>
+      </div>
+    );
+  }
+};
+
+export default login
